e93f69fa7a8b807249803130484196563a79a23b
Damon Oehlman <damon.oehlman@gmail.com>
2014-07-03 14:44:25 +1000

    Initial implementation

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+  # contain
+
+  Use [letterboxing](http://en.wikipedia.org/wiki/Letterbox) or
+  [pillarboxing](http://en.wikipedia.org/wiki/Pillar_box_(film)) to resize
+  an object to fit within another containing rect, without affecting the
+  aspect ratio.
+
+  ## Example Usage
+
+  To be completed.
+
+**/
+module.exports = function(container, target) {
+
+  function contain(target) {
+    return [];
+  }
+
+  return target ? contain(target) : contain;
+};
