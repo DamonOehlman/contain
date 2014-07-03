3da87dc163c326eb46cc8f7e5bb9ac925807b531
Damon Oehlman <damon.oehlman@gmail.com>
2014-07-03 15:22:23 +1000

    Additional letterbox

--- test/letterbox.js
+++ test/letterbox.js
@@ -3,3 +3,4 @@ var contain = require('../');
 var validate = require('./helpers/validate');
 
 test('[0,0,200,200] within [100,100,100,100] => [100,100,100,100]', validate);
+test('[0,0,200,100] within [100,100,100,100] => [100,125,100,50]', validate);
